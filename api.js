0da5e47d7a310dd4fe47c99026591f49bf2e87fd
zls1018562276 <1018562276@qq.com>
2019-12-09 14:56:04 +0800

    no message

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 import {
     wxRequest
 } from './wxRequest';
-const apiMall = 'https://erp.jizhangyl.com/jizhangyl' // 服务器端
+const apiMall = 'www.xxxx.com' // 服务器端
 //验证邀请码是否正确
 const checkInviteCode = (params) => wxRequest(params, apiMall + 'user/checkInviteCode');
 export default {
